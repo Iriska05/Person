fe3c9367cb2d1e799bdc6f0ac426a0da787d3453
Iriska05 <124791822+Iriska05@users.noreply.github.com>
2023-03-03 10:34:30 +0300

    Add files via upload

---
+++ Person2.java
@@ -0,0 +1,10 @@
+public class Person2 {
+    public static void main(String[] args) {
+        //№4
+        Person firstObject = new Person();
+        Person secondObject = new Person("Петя", 18);
+        //№5
+        firstObject.move("кто-то");
+        secondObject.talk("такой");
+    }
+}
